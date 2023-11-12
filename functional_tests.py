9a29b47d6435cd34453cec0c67a15202e2f0ed78
Sam Kwok <stckwok@hotmail.com>
2023-11-12 15:15:34 -0800

    refactor test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,35 @@
 from selenium import webdriver
+import unittest
+import time
+import warnings
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+def ignore_warnings(test_func):
+    def do_test(self, *args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore", ResourceWarning)
+            test_func(self, *args, **kwargs)
+    return do_test
 
-assert "Congratulations!" in browser.title
-print("OK")
+class VisitorPageTest(unittest.TestCase):
+    @ignore_warnings
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
+    @ignore_warnings
+    def tearDown(self):
+        self.browser.quit()
+
+    @ignore_warnings
+    def test_can_start_a_list(self):
+
+        self.browser.get("http://localhost:8000")
+
+        print("Title : ", self.browser.title)
+        assert "Congratulations!" in self.browser.title
+        print("OK")
+
+        # sleep a bit and close Firefox page
+        time.sleep(5)
+
+if __name__ == "__main__":
+    unittest.main()   # warnings='ignore'
