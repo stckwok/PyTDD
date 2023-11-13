d864c290b72a228ca6f2529335856a58655a556c
Sam Kwok <stckwok@hotmail.com>
2023-11-12 16:18:34 -0800

    url config, map / to home_page view

--- functional_tests.py
+++ functional_tests.py
@@ -20,13 +20,19 @@ class VisitorPageTest(unittest.TestCase):
         self.browser.quit()
 
     @ignore_warnings
-    def test_can_start_a_list(self):
-
+    def test_can_start_a_todolist(self):
+        # 1. Launch home page of ToDoList App
         self.browser.get("http://localhost:8000")
 
         print("Title : ", self.browser.title)
-        assert "Congratulations!" in self.browser.title
-        print("OK")
+        # assert "Congratulations!" in self.browser.title
+        # print("OK")
+
+        # 2. page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        # 3. enter a to-do item straight away
+        self.fail("Finish the test!") 
 
         # sleep a bit and close Firefox page
         time.sleep(5)
